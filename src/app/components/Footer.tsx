0453b050ab5367e08f5568e1aa4608e21a6189ff
truongthonghcmut <lqd.tk21.letruongthong@gmail.com>
2024-04-14 12:17:14 +0700

    Landing Page

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react"
+
+export default function Footer() {
+    return(
+        <footer className="footer_landing">
+        Copyright 2024 BKCare
+        </footer>
+    );
+}
