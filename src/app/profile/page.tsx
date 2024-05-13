05de36e02b29ba2074d7a98f5d3e7cbe1b5d5404
chezzijr <nhuthuan0501@gmail.com>
2024-05-13 21:06:37 +0700

    feat: profile renderer

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,7 @@ import { getCredentials } from '@/lib/creds';
 import { useEffect, useState } from 'react';
 
 export default function ProfilePage() {
-    const [profile, setProfile] = useState(null);
+    const [profile, setProfile] = useState<any>(null);
     const [error, setError] = useState('');
 
     useEffect(() => {
@@ -50,12 +50,41 @@ export default function ProfilePage() {
     }, []);
 
     return (
-        <div>
-            <h1>Profile</h1>
-            <div>
-                {JSON.stringify(profile, null, 2)}
+        <>
+            {profile && <ProfileRenderer profile={ profile } />}
+            {!profile && !error && <div className="text-center">Loading...</div>}
+            {!profile && error && <div className="text-center text-red-500">{error}</div>}
+        </>
+    );
+}
+
+function ProfileRenderer({ profile }: { profile: any }) {
+    const creds = getCredentials();
+    if (!creds) {
+        window.location.href = '/login';
+        return;
+    }
+    const role = creds.role.toLowerCase();
+    const array = Object.entries(profile[role]);
+    const items = array.filter(([key, value]) => key && value).map(([key, value]) => {
+        return (
+            <div key={key} className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">{key}</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{value}</dd>
             </div>
-            {error && <div className="text-red-500">{error}</div>}
+        );
+    })
+
+    return (
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+            <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">User Profile</h3>
+            </div>
+            <div className="border-t border-gray-200">
+                <dl>
+                    {items}
+                </dl>
+            </div>
         </div>
     );
 }
